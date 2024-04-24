12c666de01b842d4f14b5b2e3002be15f6db1ff0
PineHead94 <udhav.varma.uv@gmail.com>
2024-04-24 17:53:16 +0530

    added about section

---
+++ script.js
@@ -0,0 +1,15 @@
+let headerAnchorTag = document.getElementsByTagName("a");
+let headerBurgerMenu = document.getElementById("navi-toggle");
+
+headerAnchorTag[0].addEventListener("click", () => {
+  headerBurgerMenu.checked = false;
+});
+headerAnchorTag[1].addEventListener("click", () => {
+  headerBurgerMenu.checked = false;
+});
+headerAnchorTag[2].addEventListener("click", () => {
+  headerBurgerMenu.checked = false;
+});
+headerAnchorTag[3].addEventListener("click", () => {
+  headerBurgerMenu.checked = false;
+});
