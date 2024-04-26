7592dfbcff841df11a3f2515ef4519208484425d
PineHead94 <udhav.varma.uv@gmail.com>
2024-04-26 21:03:16 +0530

    added folder

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ headerAnchorTag[2].addEventListener("click", () => {
 headerAnchorTag[3].addEventListener("click", () => {
   headerBurgerMenu.checked = false;
 });
+
+
+const cursorRounded = document.querySelector('.rounded');
+const cursorPointed = document.querySelector('.pointed');
+// const cursorPointed = document.querySelector('.test-cursor');
+
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', moveCursor)
