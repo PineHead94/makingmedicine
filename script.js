fa4e7ecf8d55cb6e78e84fd4586b8c9bc6f3b444
PineHead94 <udhav.varma.uv@gmail.com>
2024-05-07 10:21:47 +0530

    changes

--- script.js
+++ script.js
@@ -10,24 +10,3 @@ headerAnchorTag[1].addEventListener("click", () => {
 headerAnchorTag[2].addEventListener("click", () => {
   headerBurgerMenu.checked = false;
 });
-headerAnchorTag[3].addEventListener("click", () => {
-  headerBurgerMenu.checked = false;
-});
-
-
-const cursorRounded = document.querySelector('.rounded');
-const cursorPointed = document.querySelector('.pointed');
-// const cursorPointed = document.querySelector('.test-cursor');
-
-
-const moveCursor = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-   
-  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  
-  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
-}
-
-window.addEventListener('mousemove', moveCursor)
